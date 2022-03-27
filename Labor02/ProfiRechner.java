e9912b1f99e6bf01a1086ec5e2d2e8640958ad6d
Alexander Nachtigal <a.nachtigal98@gmail.com>
2022-03-28 01:00:38 +0200

    adding the text from the tasks as a comment

--- Labor02/ProfiRechner.java
+++ Labor02/ProfiRechner.java
@@ -7,3 +7,15 @@ public class ProfiRechner{
         
     }
 }
+
+
+/* Task: 
+Könnt ihr euer Programm für die folgenden Aufgaben erweitern?
+    • Verhindert mögliche Fehler und gebt einen Hilfetext in Fehlerfall aus.
+    • Zusätzlich zu Integer-Zahlen sollen auch Fließkomma Zahlen (Double) eingegeben
+      werden können.
+    • Eine beliebige Anzahl von Parametern kann angeben werden.
+        o Beachtet dabei mögliche Fehlerfälle.
+        o Beachtet die Punkt-vor-Strich Regel (bei mehreren Parametern).
+        o Ergänzt Klammern als mögliche Eingabe.
+*/ 
--- Labor02/Taschenrechner.java
+++ Labor02/Taschenrechner.java
@@ -7,3 +7,16 @@ public class Taschenrechner{
         
     }
 }
+
+/* Task: 
+Programmiert in Java einen einfachen Taschenrechner, der drei Parameter über die
+Kommandozeile übergeben bekommt. Der erste und dritte Parameter soll eine (Integer) Zahl
+sein, der zweite Parameter eine Grundrechenart (+, -, *, /). Das Programm soll dann das
+Ergebnis dieser Rechnung ausgeben.
+
+Bsp:
+$java Calculator.java 3 + 5
+8
+$java Calculator.java 5 / 2
+2.5
+*/ 
