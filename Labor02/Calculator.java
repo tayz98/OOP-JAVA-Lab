aece2652582124b76921534d88df47544ecc06b3
Sören Zacharias <soeren.zacharias@student.fh-kiel.de>
2022-03-28 18:18:28 +0200

    Task 1 completed

--- Labor02/Calculator.java
+++ Labor02/Calculator.java
@@ -4,8 +4,22 @@ package Labor02;
 public class Calculator{
 
     public static void main(String[] args) {
-        System.out.println("Hallo Taschenrechner"); // random comment
-        System.out.println("Test.");
+        int n1 = Integer.parseInt(args[0]);
+        int n2 = Integer.parseInt(args[2]);
+        switch (args[1]) {
+            case "+" -> System.out.println(n1 + n2);
+            case "-" -> System.out.println(n1 - n2);
+            case "/" -> {
+                double result = ((double) n1) / n2;
+                if (result % 1 == 0) {
+                    System.out.println((int) result);
+                } else {
+                    System.out.println(result);
+                }
+            }
+            case "*" -> System.out.println(n1 * n2);
+            default -> System.out.println(0);
+        }
     }
 }
 
