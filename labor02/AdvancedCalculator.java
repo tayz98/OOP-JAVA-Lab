1903aca9faad0d1219c83b59d42cf7d31ae7a416
Alexander Nachtigal <a.nachtigal98@gmail.com>
2022-04-01 20:01:14 +0200

    test

--- labor02/AdvancedCalculator.java
+++ labor02/AdvancedCalculator.java
@@ -21,7 +21,7 @@ public class AdvancedCalculator {
     }
 
     public static double nextNumber(double[] numbers, int number2) {
-        for (int j = number2 - 1; j >= 0; j--) {
+        for (int j = number2; j > numbers.length; j++) {
             if (numbers[j] != 0) {
                 double temp = numbers[j];
                 numbers[j] = 0;
