54e44fc1547e4be3b98e4c5c75117f125fd26e93
Sören Zacharias <soeren.zacharias@student.fh-kiel.de>
2022-04-10 19:02:18 +0200

    Test

--- labor03/ZombieGame.java
+++ labor03/ZombieGame.java
@@ -4,7 +4,7 @@
  * @file ZombieGame.java
  * @authors Veronica Zylla, Sören Zacharias, Alexander Nachtigal
  * @email veronica.zylla@student.fh-kiel.de, soeren.zacharias@student.fh-kiel.de, alexander.nachtigal@student.fh-kiel.de
- * @description Zombie Spiel: hier geht's ums nackte Überleben!
+ * @description Zombie Spiel: hier geht's ums nackte Überleben!!
  */
 
 package labor03;
