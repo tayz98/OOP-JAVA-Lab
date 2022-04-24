ce87199807736c4399aa395f90ebfd28f40e1e0d
Sören Zacharias <soeren.zacharias@student.fh-kiel.de>
2022-04-24 13:03:46 +0200

    Zwischenstand

--- labor05/X.java
+++ labor05/X.java
@@ -1,43 +1,57 @@
+/**
+ * @author SörenZacharias
+ * @version 1
+ * @since 1
+ */
 public class X {
-  public static boolean drawBoard(final int[] A, final int B) {
-    for (int i = 0; i < B; i++)
-      if (A[i] == A[B])
+  // Funktion gibt nur true oder false zurück
+  public static boolean drawBoard(final int[] arrayIn, final int in) {
+    for (int i = 0; i < in; i++)
+      if (arrayIn[i] == arrayIn[in])
         return true;
-    for (int i = B - 1, j = A[B] - 1; i >= 0; i--, j--)
-      if (A[i] == j)
+    for (int i = in - 1, j = arrayIn[in] - 1; i >= 0; i--, j--)
+      if (arrayIn[i] == j)
         return true;
-    for (int i = B - 1, j = A[B] + 1; i >= 0; i--, j++)
-      if (A[i] == j)
+    for (int i = in - 1, j = arrayIn[in] + 1; i >= 0; i--, j++)
+      if (arrayIn[i] == j)
         return true;
     return false;
   }
 
-  public static boolean a(final int[] B, final int A) {
-    if (A == 8) {
-      drawBoard(B);
+  // Funktion gibt nur true oder false zurück
+  public static boolean a(final int[] arrayIn, final int in) {
+    if (in == arrayIn.length) {
+      drawBoard(arrayIn);
       return true;
     }
-    for (int i = 0; i < 8; i++) {
-      B[A] = i;
-      if (drawBoard(B, A))
+    for (int i = 0; i < arrayIn.length; i++) {
+      arrayIn[in] = i;
+      if (drawBoard(arrayIn, in))
         continue;
-      final boolean C = a(B, A + 1);
+      final boolean C = a(arrayIn, in + 1);
       if (C)
         return true;
     }
     return false;
   }
 
+  /**
+   * description test
+   * @param A
+   */
   public static void drawBoard(final int[] A) {
-    for (int i = 0; i < 8; i++) {
-      for (int j = 0; j < 8; j++)
+    for (int i = 0; i < A.length; i++) {
+      for (int j = 0; j < A.length; j++)
         System.out.print("|" + ((i == A[j]) ? 'D' : ' '));
       System.out.println("|");
     }
   }
 
   public static void main(final String[] args) {
-    final int[] A = { 0, 0, 0, 0, 0, 0, 0, 0 };
-    a(A, 0);
+    /**
+     * array with length 8 and filled with 0's
+     */
+    final int[] arrayInt = { 0, 0, 0, 0, 0, 0, 0, 0 };
+    a(arrayInt, 0);
   }
 }
