63ad4d6b64ac45af072769e20a9ee866a78650e4
Sören Zacharias <soeren.zacharias@student.fh-kiel.de>
2022-04-24 12:06:45 +0200

    Zwischenstand

--- labor05/X.java
+++ labor05/X.java
@@ -1,20 +1,43 @@
 public class X {
-  public static boolean A(final int[] A, final int B) {
-    for (int b = 0; b < B; b++)
-      if (A[b] == A[B])
+  public static boolean drawBoard(final int[] A, final int B) {
+    for (int i = 0; i < B; i++)
+      if (A[i] == A[B])
         return true;
-    for (int a = B - 1, b = A[B] - 1; a >= 0; a--, b--) if (A[a] == b) return true;
-    for (int b = B - 1, a = A[B] + 1; b >= 0; b--, a++) if (A[b] == a) return true;
+    for (int i = B - 1, j = A[B] - 1; i >= 0; i--, j--)
+      if (A[i] == j)
+        return true;
+    for (int i = B - 1, j = A[B] + 1; i >= 0; i--, j++)
+      if (A[i] == j)
+        return true;
     return false;
   }
 
   public static boolean a(final int[] B, final int A) {
-    if (A == 8) {A(B);return true;}
-    for (int b = 0; b < 8; b++) {B[A] = b;if (A(B, A)) continue;final boolean C = a(B, A + 1);if (C) return true;}
+    if (A == 8) {
+      drawBoard(B);
+      return true;
+    }
+    for (int i = 0; i < 8; i++) {
+      B[A] = i;
+      if (drawBoard(B, A))
+        continue;
+      final boolean C = a(B, A + 1);
+      if (C)
+        return true;
+    }
     return false;
   }
 
-  public static void A(final int[] A) {for (int a = 0; a < 8; a++) {for (int B = 0; B < 8; B++) System.out.print("|" + ((a == A[B]) ? 'D' : ' ')); System.out.println("|");}}
+  public static void drawBoard(final int[] A) {
+    for (int i = 0; i < 8; i++) {
+      for (int j = 0; j < 8; j++)
+        System.out.print("|" + ((i == A[j]) ? 'D' : ' '));
+      System.out.println("|");
+    }
+  }
 
-  public static void main(final String[] args) {final int[] A = { 0, 0, 0, 0, 0, 0, 0, 0 };a(A, 0);}
+  public static void main(final String[] args) {
+    final int[] A = { 0, 0, 0, 0, 0, 0, 0, 0 };
+    a(A, 0);
+  }
 }
